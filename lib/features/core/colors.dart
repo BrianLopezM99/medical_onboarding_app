6d2a609e5b4866cf317669512d4ee1ea0bf36cc0
BrianLopezM99 <lopez_brian_22@hotmail.com>
2025-06-17 04:49:56 -0600

    style: :lipstick: UI main menu done

---
+++ lib/features/core/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/services.dart';
+
+class ColorsCore {
+  static const greenOne = Color(0xFF353F34);
+  static const greenTwo = Color(0xFF667467);
+  static const greenThree = Color(0xFFA0BF9F);
+  static const greenFour = Color(0xFFC1D9C1);
+  static const greenFive = Color(0xFFCAD8C7);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:medical_onboarding_app/features/core/colors.dart';
 import 'package:medical_onboarding_app/features/customer/presentation/screens/customer_list_screen.dart';
 import 'package:medical_onboarding_app/features/messaging/presentation/pages/message_screen.dart';
 
@@ -28,47 +29,115 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Módulos de la prueba técnica')),
+      backgroundColor: Color(0xFF667467),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Center(
+          child: Text(
+            'Modules for the technical test',
+            style: TextStyle(
+              color: ColorsCore.greenFive,
+              fontSize: 52,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: const Icon(Icons.people),
-                title: const Text('Módulo 1: Gestión de Clientes (CRUD)'),
-                subtitle: const Text('Ir al módulo de gestión de clientes'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => const CustomerListScreen(),
-                    ),
-                  );
-                },
-              ),
-            ),
-            const SizedBox(height: 16),
-            Card(
-              child: ListTile(
-                leading: const Icon(Icons.chat_bubble_outline),
-                title: const Text('Módulo 2: Chat AI para Documentos'),
-                subtitle: const Text('Ir al chat con IA para documentos'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => const MessageScreen(
-                        customerId: 'ai_customer',
-                        customerName: 'Cliente IA',
-                        isAi: true,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                height: 220,
+                width: 1020,
+                child: Card(
+                  color: ColorsCore.greenFour,
+                  child: ListTile(
+                    title: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: const [
+                          Icon(Icons.people, size: 60), // si quieres mantenerlo
+                          Text(
+                            'Module 1: Client Management (CRUD)',
+                            style: TextStyle(
+                              color: ColorsCore.greenOne,
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            'Go to the client management module',
+                            style: TextStyle(
+                              color: ColorsCore.greenOne,
+                              fontSize: 24,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
                       ),
                     ),
-                  );
-                },
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => const CustomerListScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 120),
+              SizedBox(
+                height: 220,
+                width: 1020,
+                child: Card(
+                  color: ColorsCore.greenFour,
+                  child: ListTile(
+                    title: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(Icons.chat_bubble_outline, size: 60),
+                          const Text(
+                            'Module 2: Chat AI for Documents',
+                            style: TextStyle(
+                              color: ColorsCore.greenOne,
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const Text(
+                            'Go to the chat with AI for documents',
+                            style: TextStyle(
+                              color: ColorsCore.greenOne,
+                              fontSize: 24,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => const MessageScreen(
+                            customerId: 'ai_customer',
+                            customerName: 'Cliente IA',
+                            isAi: true,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
